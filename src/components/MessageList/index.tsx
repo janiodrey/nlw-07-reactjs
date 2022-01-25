d60db131c3d849686891d67d842ef0c6674ca78c
Janio Muianga <janiodrey@gmail.com>
2022-01-25 23:00:18 +0200

    fetching messages data from API

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,3 +1,47 @@
+import { api } from "../../services/api";
+
+import styles from "./styles.module.scss";
+import logoImg from "../../assets/logo.svg";
+import { useEffect, useState } from "react";
+
+type Message = {
+  id: string;
+  text: string;
+  user: {
+    name: string;
+    avatar_url: string;
+  };
+};
+
 export const MessageList = () => {
-  return <h1>MessageList</h1>;
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  useEffect(() => {
+    api.get<Message[]>("messages/last-3").then((response) => {
+      setMessages(response.data);
+    });
+  }, []);
+
+  return (
+    <div className={styles.messageListWrapper}>
+      <img src={logoImg} alt="DoWhile 2021" />
+
+      <ul className={styles.messageList}>
+        {messages.map((message) => {
+          <li className={styles.message} key={message.id}>
+            <p className={styles.messageContent}>{message.text}</p>
+            <div className={styles.messageUser}>
+              <div className={styles.userImage}>
+                <img
+                  src={message.user.avatar_url}
+                  alt={message.user.name}
+                ></img>
+              </div>
+              <span>{message.user.name}</span>
+            </div>
+          </li>;
+        })}
+      </ul>
+    </div>
+  );
 };
