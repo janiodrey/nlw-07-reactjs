daeffe19fc9556c43e937e2e2628898b12a934a0
Janio Muianga <janiodrey@gmail.com>
2022-01-28 17:05:04 +0200

    mostrar as mensagens na tela

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,11 +1,11 @@
+import { useEffect, useState } from "react";
 import { api } from "../../services/api";
 
 import styles from "./styles.module.scss";
 import logoImg from "../../assets/logo.svg";
-import { useEffect, useState } from "react";
 
 type Message = {
-  id: string;
+  _id: string;
   text: string;
   user: {
     name: string;
@@ -28,18 +28,20 @@ export const MessageList = () => {
 
       <ul className={styles.messageList}>
         {messages.map((message) => {
-          <li className={styles.message} key={message.id}>
-            <p className={styles.messageContent}>{message.text}</p>
-            <div className={styles.messageUser}>
-              <div className={styles.userImage}>
-                <img
-                  src={message.user.avatar_url}
-                  alt={message.user.name}
-                ></img>
+          return (
+            <li className={styles.message} key={message._id}>
+              <p className={styles.messageContent}>{message.text}</p>
+              <div className={styles.messageUser}>
+                <div className={styles.userImage}>
+                  <img
+                    src={message.user.avatar_url}
+                    alt={message.user.name}
+                  />
+                </div>
+                <span>{message.user.name}</span>
               </div>
-              <span>{message.user.name}</span>
-            </div>
-          </li>;
+            </li>
+          );
         })}
       </ul>
     </div>
